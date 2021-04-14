1a0c5462d5970853531eef34e242e0a534b95822
Omri Levia <omri@localhost.localdomain>
2021-04-14 11:36:49 -0700

    first commit for BLEspec

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+import testapi.*;
+
+module bleSpec {
+	requires testapi;
+	exports org.homi.plugin.spec;
+}
---
+++ src/main/java/org/homi/plugin/spec/BLESpec.java
@@ -0,0 +1,12 @@
+package org.homi.plugin.spec;
+import testapi.*;
+public enum BLESpec implements ISpecification{
+
+	CONNECT,
+	DISCONNECT,
+	PAIR,
+	READ,
+	WRITE;
+	
+
+}
